5d7b7658692923e028fc56cb4079f9075a0c88d5
vacoule <salmanhossain11222626@hotmail.com>
2023-03-28 21:03:44 +0600

    header added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-
+import logo from '../../images/Logo.svg';
+import './header.css'
 const Header = () => {
     return (
-        <div className='header'>
-            <img src="" alt="" />
+        <nav className='header'>
+            <img src={logo} alt="" />
             <div className="header-link">
-                <a href="/order">order</a><a href="/order-review">order review</a><a href="/inventory">manage inventory</a>
+                <a href="/order">order</a>
+                <a href="/order-review">order review</a>
+                <a href="/inventory">manage inventory</a>
                 <a href="/login">login</a>
             </div>
-        </div>
+        </nav>
     );
 };
 
